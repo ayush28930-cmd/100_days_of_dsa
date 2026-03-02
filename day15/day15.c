d29bda90252ca6bb3bac2f40b75cc88d55b50e05
ayush28930-cmd <ayush28930@gmail.com>
2026-03-02 22:56:21 +0530

    100 days if dsa

---
+++ day15/day15.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n, i, j;
+    int sum = 0;
+
+    
+    scanf("%d %d", &m, &n);
+
+    int a[m][n];
+
+    
+    for(i = 0; i < m; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+
+    for(i = 0; i < m && i < n; i++)
+    {
+        sum += a[i][i];
+    }
+
+    
+    printf("%d", sum);
+
+    return 0;
+}
